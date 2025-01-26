17deba11b6c0c2c64ed354527f230269a7448fc3
MAROUAN17 <marinosaglagal25@gmail.com>
2025-01-26 18:43:34 +0100

    detect input event

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 addEventListener('load', init);
 
-let wordInput = document.querySelector("#word-input");
 let currentWord = document.querySelector("#current-word");
 let scoreDisplay = document.querySelector("#score");
 let timeDisplay = document.querySelector("#time");
 let message = document.querySelector("#message");
-let seconds = document.querySelector("#seconds");
 let easyBtn = document.querySelector("#easy");
 let mediumBtn = document.querySelector("#medium");
 let hardBtn = document.querySelector("#hard");
+let nbrMistakesC = document.querySelector("#nbr-mistakes");
+let currentLevelC = document.querySelector("#current-level");
+let currentWordContainer = document.querySelector("#current-word-container");
 
 
+let nbrMistakes = 0;
 let levels = {
     "easy": {seconds: 5, wordLength: 5},
     "medium": {seconds: 3, wordLength: 8},
@@ -21,6 +23,7 @@ let currentLevel = levels.easy;
 let timer = currentLevel.seconds;
 let isPlaying = false;
 let score = 0;
+let wordToCompare = '';
 
 easyBtn.addEventListener("click", () => {
     if (!isPlaying) {
@@ -45,20 +48,23 @@ async function init() {
     try {
         const res = await fetch("https://random-word-api.herokuapp.com/all");
         words = await res.json();
-        seconds.textContent = `you have ${currentLevel.seconds} seconds to type the word`;
         let randIndex = Math.floor(Math.random() * words.length);
         console.log(currentLevel.wordLength);
         while (words[randIndex].length > currentLevel.wordLength) {
             randIndex = Math.floor(Math.random() * words.length);
         }
         currentWord.textContent = words[randIndex];
-        scoreDisplay.textContent = `score : ${score}`;
+        scoreDisplay.textContent = `${score}`;
         setInterval(countdown, 1000);
         setInterval(checkGameStatus, 50);
     } catch(err) {
         console.log(err.message);
     }
 }
+
+currentWordContainer.addEventListener('keyup', (ev) => {
+    startMatch(ev.key);
+});
 
 function generateWords() {
     let randIndex = Math.floor(Math.random() * words.length);
@@ -68,17 +74,19 @@ function generateWords() {
     currentWord.textContent = words[randIndex];
     console.log("word length : ", currentWord.textContent.length);
     isPlaying = true;
-    wordInput.value = "";
+    wordToCompare = "";
+    i = 0;
     message.textContent = "";
     timer = currentLevel.seconds;
     score++;
-    scoreDisplay.textContent = `score : ${score}`;
+    scoreDisplay.textContent = `${score}`;
 }
 
 addEventListener("input", startMatch);
 
-function startMatch() {
-    if (matchWords()) {
+function startMatch(key) {
+    console.log(key)
+    if (matchWords(key)) {
         console.log("CORRECT!!");
         generateWords();
     }
@@ -86,22 +94,28 @@ function startMatch() {
         console.log("WRONG!!");
     }
 }
-
-function matchWords() {
+function matchWords(key) {
     let colorChar = '';
+    if (key == "Backspace")
+        wordToCompare = wordToCompare.slice(0, -1);
+    else
+        wordToCompare += key;
+    console.log("string after slicing :", wordToCompare);
     for (let i = 0; i < currentWord.textContent.length; i++)  {
-        if (i < wordInput.value.length) {
-            if (wordInput.value.charAt(i) === currentWord.textContent.charAt(i))
+        if (i < wordToCompare.length) {
+            if (wordToCompare.charAt(i) === currentWord.textContent.charAt(i))
                 colorChar += `<span class="text-green-500">${currentWord.textContent.charAt(i)}</span>`;
-            else
+            else {
                 colorChar += `<span class="text-red-500">${currentWord.textContent.charAt(i)}</span>`;
+                nbrMistakes++;
+            }
         }
         else {
             colorChar += `<span class="text-white">${currentWord.textContent.charAt(i)}</span>`;
         }
     }
     currentWord.innerHTML = colorChar;
-    if (wordInput.value === currentWord.textContent)
+    if (wordToCompare === currentWord.textContent)
         return (true);
     else
         return (false);
@@ -116,9 +130,16 @@ function countdown() {
 
 function checkGameStatus() {
     timeDisplay.textContent = timer;
-    seconds.textContent = `you have ${currentLevel.seconds} seconds to type the word`;
+    nbrMistakesC.textContent = nbrMistakes;
+    if (currentLevel.seconds == 5)
+        currentLevelC.textContent = "Easy";
+    else if (currentLevel.seconds == 3)
+        currentLevelC.textContent = "Medium";
+    else if (currentLevel.seconds == 2)
+        currentLevelC.textContent = "Hard";
     if (!isPlaying && timer == 0) {
         message.textContent = "Game Over";
+        nbrMistakes = 0;
         score = 0;
     }
 }
